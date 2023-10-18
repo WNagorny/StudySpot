87f1f32da1c9e94891356fffc47c1d4eecb96d9c
WNagorny <walery.nagorny@gmail.com>
2023-10-18 17:12:36 +0200

    created a folder structure

--- app/(dashboard)/(routes)/page.tsx
+++ app/(dashboard)/(routes)/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <p className='text-3xl font-medium text-sky-700'>Hello</p>
+    <p className='text-3xl font-medium text-sky-700'>This is protected page </p>
   )
 }
