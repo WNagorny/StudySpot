be30a29d69d1ad62475ae6ac4843b0a6824b97a7
WNagorny <walery.nagorny@gmail.com>
2023-10-23 17:05:35 +0200

    added a Price-form component

--- app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
+++ app/(dashboard)/(routes)/teacher/courses/[courseId]/page.tsx
@@ -1,14 +1,20 @@
 import { auth } from '@clerk/nextjs';
 import { redirect } from 'next/navigation';
-import { CircleDollarSign, File, LayoutDashboard, ListChecks } from "lucide-react";
+import {
+	CircleDollarSign,
+	File,
+	LayoutDashboard,
+	ListChecks,
+} from 'lucide-react';
 
 import { db } from '@/lib/db';
-import { IconBadge } from "@/components/icon-badge";
+import { IconBadge } from '@/components/icon-badge';
 
 import { TitleForm } from './_components/title-form';
 import { DescriptionForm } from './_components/description-form';
 import { ImageForm } from './_components/image-form';
 import { CategoryForm } from './_components/category-form';
+import { PriceForm } from './_components/price-form';
 
 const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	const { userId } = auth();
@@ -24,12 +30,10 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 	});
 
 	const categories = await db.category.findMany({
-		orderBy:{
-			name: "asc",
+		orderBy: {
+			name: 'asc',
 		},
-	})
-
-	
+	});
 
 	if (!course) {
 		return redirect('/');
@@ -64,29 +68,41 @@ const CourseIdPage = async ({ params }: { params: { courseId: string } }) => {
 			<div className='grid grid-cols-1 md:grid-cols-2 gap-6 mt-16'>
 				<div>
 					<div className='flex items-center gap-x-2'>
-               <IconBadge icon={LayoutDashboard} />
+						<IconBadge icon={LayoutDashboard} />
 						<h2 className='text-xl'>Customize your course</h2>
 					</div>
-               <TitleForm
-              initialData={course}
-              courseId={course.id}
-            />
-            <DescriptionForm
-              initialData={course}
-              courseId={course.id}
-            />
-            <ImageForm
-              initialData={course}
-              courseId={course.id}
-            />
-             <CategoryForm
-              initialData={course}
-              courseId={course.id}
-              options={categories.map((category) => ({
-                label: category.name,
-                value: category.id,
-              }))}
-            />
+					<TitleForm initialData={course} courseId={course.id} />
+					<DescriptionForm initialData={course} courseId={course.id} />
+					<ImageForm initialData={course} courseId={course.id} />
+					<CategoryForm
+						initialData={course}
+						courseId={course.id}
+						options={categories.map(category => ({
+							label: category.name,
+							value: category.id,
+						}))}
+					/>
+				</div>
+
+				<div className='space-y-6'>
+					<div>
+						<div className='flex items-center gap-x-2'>
+							<IconBadge icon={ListChecks} />
+
+							<h2 className='text-xl'>Course chapters</h2>
+						</div>
+						<div>
+							TODO: Chapters
+						</div>
+					</div>
+
+					<div>
+						<div className='flex items-center gap-x-2'>
+						<IconBadge icon={CircleDollarSign} />
+							<h2 className='text-xl'>Sell your course</h2>
+						</div>
+						<PriceForm initialData={course} courseId={course.id}/>
+					</div>
 				</div>
 			</div>
 		</div>
