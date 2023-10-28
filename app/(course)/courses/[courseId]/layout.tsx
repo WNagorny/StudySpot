e63bf70b1f402783ee08c076dc3a4006c25c690b
WNagorny <walery.nagorny@gmail.com>
2023-10-28 09:50:14 +0200

    added a Course-Sidebar-item

--- app/(course)/courses/[courseId]/layout.tsx
+++ app/(course)/courses/[courseId]/layout.tsx
@@ -1,60 +1,60 @@
-import { auth } from "@clerk/nextjs";
-import { redirect } from "next/navigation";
+import { auth } from '@clerk/nextjs';
+import { redirect } from 'next/navigation';
 
-import { db } from "@/lib/db";
-import { getProgress } from "@/actions/get-progress";
-
+import { db } from '@/lib/db';
+import { getProgress } from '@/actions/get-progress';
+import { CourseSidebar } from './_components/course-sidebar';
 
 const CourseLayout = async ({
-  children,
-  params
+	children,
+	params,
 }: {
-  children: React.ReactNode;
-  params: { courseId: string };
+	children: React.ReactNode;
+	params: { courseId: string };
 }) => {
+	const { userId } = auth();
 
-   const { userId } = auth();
+	if (!userId) {
+		return redirect('/');
+	}
 
-   if (!userId) {
-     return redirect("/")
-   }
- 
-   const course = await db.course.findUnique({
-     where: {
-       id: params.courseId,
-     },
-     include: {
-       chapters: {
-         where: {
-           isPublished: true,
-         },
-         include: {
-           userProgress: {
-             where: {
-               userId,
-             }
-           }
-         },
-         orderBy: {
-           position: "asc"
-         }
-       },
-     },
-   });
- 
-   if (!course) {
-     return redirect("/");
-   }
- 
-   const progressCount = await getProgress(userId, course.id);
- 
+	const course = await db.course.findUnique({
+		where: {
+			id: params.courseId,
+		},
+		include: {
+			chapters: {
+				where: {
+					isPublished: true,
+				},
+				include: {
+					userProgress: {
+						where: {
+							userId,
+						},
+					},
+				},
+				orderBy: {
+					position: 'asc',
+				},
+			},
+		},
+	});
 
-  return (
-    <div className="h-full">
-      {children}
-    </div>
-  )
-}
+	if (!course) {
+		return redirect('/');
+	}
 
-export default CourseLayout
+	const progressCount = await getProgress(userId, course.id);
 
+	return (
+		<div className='h-full'>
+			<div className='hidden md:flex h-full w-80 flex-col fixed inset-y-0 z-50'>
+				<CourseSidebar course={course} progressCount={progressCount} />
+			</div>
+			<main className='md:pl-80 pt-[80px] h-full'>{children}</main>
+		</div>
+	);
+};
+
+export default CourseLayout;
