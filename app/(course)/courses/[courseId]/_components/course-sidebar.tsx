1056bff24ab6c12183f511d35be4ae855bb83101
WNagorny <walery.nagorny@gmail.com>
2023-10-28 10:01:14 +0200

    added a Course-navabr and mobile-sidebar

--- app/(course)/courses/[courseId]/_components/course-sidebar.tsx
+++ app/(course)/courses/[courseId]/_components/course-sidebar.tsx
@@ -36,7 +36,7 @@ export const CourseSidebar = async ({
 
   return (
     <div className="h-full border-r flex flex-col overflow-y-auto shadow-sm">
-      <div className="p-8 flex flex-col border-b">
+      <div className="p-6 flex items-center flex-col border-b">
         <h1 className="font-semibold">
           {course.title}
         </h1>
