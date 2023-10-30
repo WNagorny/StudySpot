d9aee4014dd51bf885b02b91a285fbfa89e4bc8c
WNagorny <walery.nagorny@gmail.com>
2023-10-30 17:55:58 +0100

    updated components imports

--- app/(dashboard)/(routes)/teacher/analytics/page.tsx
+++ app/(dashboard)/(routes)/teacher/analytics/page.tsx
@@ -2,10 +2,10 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import { getAnalytics } from "@/actions/get-analytics";
-
 import { DataCard } from "./_components/data-card";
 import { Chart } from "./_components/chart";
-// import { Chart } from "./_components/chart";
+
+
 
 const AnalyticsPage = async () => {
   const { userId } = auth();
